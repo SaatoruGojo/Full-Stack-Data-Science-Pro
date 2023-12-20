a00e9ea91c80c344432d7cee27329f4dbcffbabd
Harsh Anand <harshanand23561@gmail.com>
2023-12-20 18:09:45 +0530

    Oath

---
+++ Flask Project/flask-google-login/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, redirect
+from authlib.integrations.flask_client import OAuth
+import os
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)
+
+oauth = OAuth(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/google/')
+def google():
+    GOOGLE_CLIENT_ID = '75842008522-u1qbmhpmvsiri58rc5lmtd564akgn7ag.apps.googleusercontent.com'
+    GOOGLE_CLIENT_SECRET = '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'
+
+    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+    oauth.register(
+        name='google',
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+
+    # Redirect to google_auth function
+    redirect_uri = url_for('google_auth', _external=True)
+    print(redirect_uri)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    user = oauth.google.parse_id_token(token)
+    print(" Google User ", user)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
